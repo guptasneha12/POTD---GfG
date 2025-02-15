c8959a31e628666ec088a7520ba350dcece30aed
Sneha Gupta <guptasneha2003.sg@gmail.com>
2025-02-15 12:51:59 +0530

    Add initial C++ code

---
+++ Lowest Common Ancestor in a BST.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    Node* LCA(Node* root, Node* n1, Node* n2) {
+        if(!root) return NULL;
+        if(n1->data > root->data && n2->data > root->data){
+           return LCA(root->right,n1,n2);
+        }
+        if(n1->data < root->data && n2->data < root->data){
+             return LCA(root->left,n1,n2);
+        }
+        return root;
+    }
+};
