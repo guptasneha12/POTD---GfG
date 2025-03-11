70cc555a94c7764e9bfe35ec608aeb95ff422442
Sneha Gupta <guptasneha2003.sg@gmail.com>
2025-03-11 12:35:01 +0530

    Add initial C++ code

---
+++ Ways to Reach the n'th Stair.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int countWays(int n) {
+        // your code here
+        int prev1=1;
+        int prev2=1;
+       
+        for(int i=2;i<=n;i++){
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};
