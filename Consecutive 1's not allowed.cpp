473b780e42c54621f9827beffa3cdbfad3d7dd03
guptasneha12 <guptasneha2003.sg@gmail.com>
2026-04-01 22:40:25 +0530

    gfg potd

---
+++ Consecutive 1's not allowed.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int countStrings(int n) {
+        int zeroend=1;
+        int oneend=1;
+        int sum=zeroend+oneend;
+        if(n==1) return sum;
+        int i=2;
+        while(i<=n){
+            oneend=zeroend;
+            zeroend=sum;
+            sum=oneend+zeroend;
+            i++;
+        }
+        return sum;
+        
+    }
+};
