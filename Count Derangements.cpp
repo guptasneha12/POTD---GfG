71af2a397fbdb0de092058178765532684fec60d
guptasneha12 <guptasneha2003.sg@gmail.com>
2026-04-20 23:11:52 +0530

    gfg potd

---
+++ Count Derangements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int derangeCount(int n) {
+        // code here
+        int prev2=0;
+        int prev1=1;
+        if(n==1) return 0;
+        if(n==2) return 1;
+        for(int i=3;i<=n;i++){
+            int curr=(i-1)*(prev1+prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};
